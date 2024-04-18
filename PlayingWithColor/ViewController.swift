41b985d1e766369b6ef39f8ef0533508c69b25db
DenisAlexandrov <aie-dinis2007@yandex.ru>
2024-04-18 23:24:53 +0300

    Outlet connection

--- PlayingWithColor/ViewController.swift
+++ PlayingWithColor/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+        setupSliders()
+        updateColor()
     }
-
-
+    
+    func setupView() {
+        colorView.layer.cornerRadius = 20
+    }
+    
+    
+    func setupSliders() {
+            redSlider.minimumTrackTintColor = .red
+            greenSlider.minimumTrackTintColor = .green
+//            blueSlider.minimumTrackTintColor = .blue
+        }
+    
+    @IBAction func sliderValue(_ sender: UISlider) {
+        updateColor()
+    }
+    
+    func updateColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        
+        )
+    }
+    
 }
 
